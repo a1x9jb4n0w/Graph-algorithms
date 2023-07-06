05fb8afd43f786e30d37b34aa5688809063cb66d
a1x9jb4n0w <93477125+a1x9jb4n0w@users.noreply.github.com>
2023-07-06 10:04:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,20 +14,21 @@ class Graph { int esize, nsize;
     }
 
     class Node { public: int x;
-                         char *imx;
-                         bool cchecked = false;
+                         double *imx; // a line from the graph's adjacency matrix, contains weights
+                         bool uchecked = false;
+                         double dpath = INFINITY;
 
                  Node ( int _x, int _esize ) : x {_x}
 
-                 { imx = new char[_esize]{}; for ( int i = 0; i <= _esize; i++ ) imx[i] = 0; } };
+                 { imx = new double[_esize]{}; for ( int i = 0; i <= _esize; i++ ) imx[i] = 0.0; }
 
-    class Edge { public: int from, to;
+                 void clearmem() { delete[] imx; } };
+
+    class Edge { public: int fr, to;
                          double weight = 0;
-                         bool wchecked = false, cchecked = false;
+                         bool wchecked = false, uchecked = false;
 
-        Edge( int _f, int _t ) : from {_f}, to {_t}
-
-        { weight = static_cast<double>(randomize(1,1000)) * 0.01; }
+                 Edge( int _f, int _t, double _w ) : fr {_f}, to {_t}, weight {_w} {}
 
     };
 
@@ -36,44 +37,41 @@ class Graph { int esize, nsize;
 
     public:
 
-    bool iscycle() // searching for a cycle ->
+    bool iscycle() // searching for a cycle using dfs ( used by constructor & Prim ) ->
     {
         bool cycle = false;
 
-        class dfs { public: int e; dfs( int _e ) : e{_e} {}
+        class dfs { public: int esize; dfs( int _e ) : esize{_e} {}
 
-            void operator()( bool &cycle, std::vector<Node> &nodes, Node &n0, int prev )
+            void operator()( bool &cycle, std::vector<Node> &nodes, Node &n, int prev )
 
-            {   if ( n0.cchecked ) cycle = true; else { n0.cchecked = true;
+            {   if ( n.uchecked ) cycle = true; else { n.uchecked = true;
 
-                for ( int n = 0; n <= e; n++ ) {
+                for ( int i = 0; i <= esize; i++ ) {
 
-                    if ( n0.imx[n] == 1 && n != prev )
+                    if ( n.imx[i] > 0.0 && i != prev )
 
-                        this->operator()( cycle, nodes, nodes[n], n0.x );
+                        this->operator()( cycle, nodes, nodes[i], n.x );
 
-
-                    if ( cycle  ) break; }
+                    if ( cycle ) break; }
 
                 }
             }
         };
 
-        for ( auto &n0 : nodes ) { dfs itNodes{esize};
+        for ( auto &n : nodes ) { dfs itNodes{esize};
 
-            if ( !n0.cchecked ) {
+            if ( !n.uchecked ) {
 
-                itNodes.operator()( cycle, nodes, n0, 99 ); if ( cycle ) break; }
+                itNodes.operator()( cycle, nodes, n, 99999 ); if ( cycle ) break; }
         }
 
-        for ( auto &n0 : nodes ) n0.cchecked = false;
-
-        if ( cycle ) std::cout << "CYCLE ! -> ";
+        for ( auto &n0 : nodes ) n0.uchecked = false;
 
         return cycle;
     }
 
-    // creates a random graph with esize vertices and nsize edges ->
+    // constructs a random graph with esize vertices and nsize edges ->
 
     Graph( int _esize, int _nsize ) : esize {_esize}, nsize {_nsize}
     {       
@@ -83,36 +81,37 @@ class Graph { int esize, nsize;
 
         for ( int n = 0; n <= esize; n++ ) nodes.push_back( Node{n, esize} );
 
-        for ( int i = 0; i <= nsize; i++ ) { bool yesno = true; int from2, to2;
+        for ( int i = 0; i <= nsize; i++ ) { bool wrong = true; int fr, to;
 
-            while ( yesno ) {  _re :
+            double weight = static_cast<double>(randomize(1,1000)) * 0.01;;
 
-                from2 = ( i < esize ) ? i : randomize( 0, esize );
-                to2 = ( i < esize ) ? randomize( 0, esize ) : randomize( 0, esize );
+            while ( wrong ) {  _re :
 
-                if ( to2 == from2 ) goto _re; // ?
+                fr = ( i < esize ) ? i : randomize( 0, esize );
+                to = ( i < esize ) ? randomize( 0, esize ) : randomize( 0, esize );
 
-            if ( i > 0 ) { for ( auto& e : edges ) {
+                if ( to == fr ) goto _re; // ...
 
-                yesno = ( ( to2 == e.from && from2 == e.to ) || ( to2 == e.to && from2 == e.from ) ) ?
+                if ( !i ) wrong = false; else {
 
-                        true : false; if ( yesno ) break; } } else yesno = false;
+                   for ( auto& e : edges ) {
 
-            if ( i < esize && !yesno ) {
+                     wrong = ( ( to == e.fr && fr == e.to ) || ( to == e.to && fr == e.fr ) ) ? true : false;
 
-            nodes[from2].imx[to2] = 1; nodes[to2].imx[from2] = 1;
+                     if ( wrong ) break; } }
 
-            yesno = iscycle();
+                if ( !wrong ) { nodes[fr].imx[to] = weight; nodes[to].imx[fr] = weight;
 
-            nodes[from2].imx[to2] = 0; nodes[to2].imx[from2] = 0;
+                   if ( i < esize && iscycle() ) { wrong = true;
 
-            if ( yesno ) std::cout << from2 << " : " << to2 << std::endl; }
+                      nodes[fr].imx[to] = 0.0; nodes[to].imx[fr] = 0.0;
 
-            } nodes[from2].imx[to2] = 1; nodes[to2].imx[from2] = 1;
+                      std::cout << "[ CYCLE! -> " << fr << " : " << to << " ]" << std::endl; }
+                }
 
-            edges.push_back( Edge{ from2, to2 } );
+            } edges.push_back( Edge{ fr, to, weight } );
 
-            std::cout << from2 << " - " << to2 << " : " << edges.back().weight << std::endl;
+            std::cout << fr << " - " << to << " : " << edges.back().weight << std::endl;
 
         }
 
@@ -122,9 +121,11 @@ class Graph { int esize, nsize;
 
     void Prim()
     {
-        for ( auto &n0 : nodes ) for ( int i = 0; i <= esize; i++ ) n0.imx[i] = 0;
+        for ( auto &n : nodes ) { n.uchecked = false; // spoils adjacency matrix for further uses! ->
 
-        std::cout << std::endl;
+            for ( int i = 0; i <= esize; i++ ) n.imx[i] = 0; }
+
+        std::cout << std::endl << "Minimum spanning tree : "<< std::endl;
 
         if ( esize - 1 == nsize ) {
 
@@ -132,49 +133,114 @@ class Graph { int esize, nsize;
 
         int ecnt = 0;
 
-        while ( ecnt < esize ) { int pos = 0, ind = 0; double minw = 99;
+        while ( ecnt < esize ) { int pos = 0, ind = 0; double minw = INFINITY;
 
-            for ( auto& e0 : edges ) {
+            for ( auto& e : edges ) {
 
-                if ( e0.weight < minw && !e0.wchecked ) { minw = e0.weight; pos = ind; } ind++;
+                if ( e.weight < minw && !e.wchecked ) { minw = e.weight; pos = ind; } ind++;
 
             } edges.at(pos).wchecked = true;
 
-            nodes[edges.at(pos).from].imx[edges.at(pos).to] = 1;
-            nodes[edges.at(pos).to].imx[edges.at(pos).from] = 1;
+            nodes[edges.at(pos).fr].imx[edges.at(pos).to] = 1.0;
+            nodes[edges.at(pos).to].imx[edges.at(pos).fr] = 1.0;
 
-            if ( iscycle() ) { std::cout << minw << std::endl;
+            if ( iscycle() ) { std::cout << " [ CYCLE! -> " << minw << " ]" << std::endl;
 
-                nodes[edges.at(pos).from].imx[edges.at(pos).to] = 0;
-                nodes[edges.at(pos).to].imx[edges.at(pos).from] = 0;
+                nodes[edges.at(pos).fr].imx[edges.at(pos).to] = 0.0;
+                nodes[edges.at(pos).to].imx[edges.at(pos).fr] = 0.0;
 
             } else { presult.push_back( edges.at(pos) ); ecnt++; }
 
+        } for ( auto &e : presult ) std::cout << " | " << e.weight;
+
+        std::cout << " |" << std::endl;
+
+    }
+
+    void dist2all( int x ) // distances from x to every other vertice ( used by Dijkstra and centrVec )
+    {
+        int vcnt = esize;
+
+        nodes[x].dpath = 0;
+
+        while ( vcnt >= 0 ) { int cur = 0; double minp = INFINITY;
+
+          for ( int i = 0; i <= esize; i++ ) {
+
+              if ( !nodes[i].uchecked && nodes[i].dpath < minp ) { cur = i; minp = nodes[i].dpath; } }
+
+          for ( int i = 0; i <= esize; i++ ) {
+
+              if ( nodes[cur].imx[i] > 0.0 ) { double m = nodes[cur].dpath + nodes[cur].imx[i];
+
+                  if ( m < nodes[i].dpath ) nodes[i].dpath = m; } }
+
+          nodes[cur].uchecked = true; vcnt--; }
+
+    }
+
+    void Dijkstra() // searching the shortest path
+    {
+        int fr, to;
+
+          _re : fr = randomize( 0, esize );
+                to = randomize( 0, esize ); if ( to == fr ) goto _re;
+
+        std::cout << std::endl << "Shortest path from " << fr << " to " << to << " :" << std::endl;
+
+        dist2all( to );
+
+        std::vector<int> spath; spath.push_back(fr); int prev = -1;
+
+        while ( spath.back() != to ) {
+
+            for ( int i = 0; i <= esize; i++ ) {
+
+                if ( nodes[spath.back()].imx[i] > 0.0 && i != prev ) {
+
+                    if ( nodes[i].dpath + nodes[spath.back()].imx[i] == nodes[spath.back()].dpath )
+
+                    { prev = spath.back(); spath.push_back(i); } }
+
+            }
         }
 
-        for ( auto &p0 : presult ) std::cout << " : " << p0.weight << " : ";
-
-        std::cout << std::endl;
+        for ( auto &n : spath ) std::cout << " " << n << " -";
+        std::cout << "> total distance : " << nodes[fr].dpath << std::endl;
 
     }
 
-    void Dijkstra()
+    void centrVec()
     {
+        double resdist = INFINITY; int resvec = -1;
 
+        for ( int i = 0; i <= esize; i++ ) { double maxdist = 0;
+
+            for ( auto &n : nodes ) { n.dpath = INFINITY; n.uchecked = false; }
+
+            dist2all( i );
+
+            for ( auto &n : nodes ) { if ( maxdist < n.dpath ) maxdist = n.dpath; }
+
+            if ( maxdist < resdist ) { resdist = maxdist; resvec = i; }
+
+        } std::cout << std::endl << "Center of the graph : " << std::endl << resvec << std::endl;
 
     }
 
-    ~Graph() { for ( auto &n0 : nodes ) delete[] n0.imx; }
+    ~Graph() { for ( auto &n0 : nodes ) n0.clearmem(); }
 
 };
 
 int main()
 {
-    try { Graph g { 4, 6 };
-
-          g.Prim();
+    try { Graph g { 4, 8 };
 
           g.Dijkstra();
+
+          g.centrVec();
+
+          g.Prim();
 
     } catch ( std::runtime_error e ) { std::cout << e.what() << std::endl; }
 
